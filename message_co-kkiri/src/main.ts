6fb153cb6f80f8886e61b61ae86369d96b5a7ddc
nakyoung98 <jay3925@cau.ac.kr>
2024-05-05 17:54:21 +0900

    Feat: RabbitMQ Microservice 생성

--- message_co-kkiri/src/main.ts
+++ message_co-kkiri/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://localhost:5672`],
+        queue: 'messages_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
+  await app.listen();
+  Logger.log('Service Start');
 }
 bootstrap();
