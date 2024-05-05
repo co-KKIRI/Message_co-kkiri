b4282abd84ca7169b80d15a7287ba1494372f3d2
nakyoung98 <jay3925@cau.ac.kr>
2024-05-05 19:06:34 +0900

    Feat: RabbitMQ 연결

--- message_co-kkiri/src/main.ts
+++ message_co-kkiri/src/main.ts
@@ -2,23 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [`amqp://localhost:5672`],
-        queue: 'messages_queue',
-        queueOptions: {
-          durable: false,
-        },
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const user = configService.get('RABBITMQ_USER');
+  const password = configService.get('RABBITMQ_PASSWORD');
+  const host = configService.get('RABBITMQ_HOST');
+  const queueName = configService.get('RABBITMQ_QUEUE_NAME');
+
+  //TODO:
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqps://${user}:${password}@${host}`],
+      queue: queueName,
+      queueOptions: {
+        durable: false,
       },
     },
-  );
+  });
 
-  await app.listen();
+  await app.startAllMicroservices();
+
   Logger.log('Service Start');
 }
 bootstrap();
