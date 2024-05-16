b4f5e6c5b5d31a36b50092c3d06d99f49987486c
nakyoung98 <jay3925@cau.ac.kr>
2024-05-16 18:47:39 +0900

    Feat: pustMessage 컨트롤러 구현

--- message_co-kkiri/apps/manage-push-app/src/manage-push-app.controller.ts
+++ message_co-kkiri/apps/manage-push-app/src/manage-push-app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { ManagePushAppService } from './manage-push-app.service';
 
 @Controller()
 export class ManagePushAppController {
   constructor(private readonly managePushAppService: ManagePushAppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.managePushAppService.getHello();
+  @Post()
+  pushMessage(): Promise<void> {
+    this.managePushAppService.pushMessage();
+
+    return;
   }
 }
