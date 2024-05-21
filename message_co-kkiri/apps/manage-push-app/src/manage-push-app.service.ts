b5f432ffdec7ea540c847400338f5b95c5854f60
nakyoung98 <jay3925@cau.ac.kr>
2024-05-21 15:41:43 +0900

    Feat: pushMessage 코드 초기 구현

--- message_co-kkiri/apps/manage-push-app/src/manage-push-app.service.ts
+++ message_co-kkiri/apps/manage-push-app/src/manage-push-app.service.ts
@@ -1,8 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, LoggerService } from '@nestjs/common';
+import { messaging } from 'firebase-admin';
+import { FirebaseService } from './firebase/firebase.service';
+import { TokenMessage } from 'firebase-admin/lib/messaging/messaging-api';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ManagePushAppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private firebaseService: FirebaseService,
+    @Inject(Logger) private readonly logger: LoggerService,
+    private readonly configService: ConfigService,
+  ) {
+    this.pushMessage();
+  }
+
+  async pushMessage(
+    tokens: Array<string>,
+    title: string,
+    body: string,
+    link: string,
+    data: Object,
+  ) {
+    // This registration token comes from the client FCM SDKs.
+    const registrationToken = this.configService.get<string>(`TEST_TOKEN`);
+
+    const message: TokenMessage = {
+      data: {
+        score: '850',
+        time: '2:45',
+      },
+      token: registrationToken,
+      notification: {
+        title: '테스트코드에요',
+        body: '테스트코드 본문이에요',
+      },
+      webpush: {
+        fcmOptions: {
+          link: 'http://localhost',
+        },
+      },
+    };
+
+    try {
+      const response = await messaging().send(message);
+      this.logger.log(`Successfully sent message: ${response}`);
+    } catch (error) {
+      this.logger.error(`Error sending message:${error}`);
+    }
   }
 }
